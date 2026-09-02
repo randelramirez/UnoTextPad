c2fd2af8c19f53686d14da39d8aef32357fb3945
randelramirez <randelramirez1@gmail.com>
2026-09-02 20:47:12 +0800

    prefer explicit usings

--- UnoTextPad.Tests/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-global using UnoTextPad.Features.Documents;
-global using UnoTextPad.Features.Session;
-global using UnoTextPad.Features.Settings;
-global using UnoTextPad.Infrastructure.Storage;
-global using UnoTextPad.Tests.TestInfrastructure;
-global using Xunit;
-global using static UnoTextPad.Tests.TestInfrastructure.TestCancellation;
